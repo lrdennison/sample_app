158700907b7752fce8d1dd676d25ab2e4a8786ac
Larry Dennison <ldennison@metratech.com>
2013-03-10 09:51:15 -0400

    Authorization added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -16,6 +19,37 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+    # @user = User.find(params[:id])
+  end
+
+  def update
+    # @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  # LRD: I don't like this method name
+  def signed_in_user
+    if( !signed_in?)
+      store_location
+      flash[:notice] = "Please sign in."
+      redirect_to signin_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 
 end
