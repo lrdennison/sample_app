332014ed9c74bba3a2b55654c2a293939ada6480
Larry Dennison <ldennison@metratech.com>
2013-03-10 16:27:55 -0400

    Some more static page tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,11 +20,42 @@ describe "StaticPages" do
 
   describe "Help Page" do
     before { visit help_path }
-    let(:heading)    { 'Sample App' }
-    let(:page_title) { '' }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    # it_should_behave_like "all static pages"
-    it { should have_selector 'title', text: '| Help' }
+    it_should_behave_like "all static pages"
+  end
+
+  describe "About Us" do
+    before { visit about_path }
+    let(:heading)    { 'About Us' }
+    let(:page_title) { 'About Us' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "Contact" do
+    before { visit contact_path }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    page.should have_selector 'title', text: full_title('')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
   end
 
 end
